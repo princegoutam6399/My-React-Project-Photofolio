7d2459b4f34ad3b33a5bbbfa6921b4f76a531990
Prince <princegoutam65101@gmail.com>
2024-03-01 11:45:50 +0530

    I am change my file

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -5,9 +5,111 @@ import Footer from '../Common/Footer'
 export default function Gallery() {
   return (
     <div>
-      <Header/>
-          <h2>Gallery page</h2>
-      <Footer/>
-      </div>
+      <Header />
+      <h2 className='text-center pt-5'></h2>
+      <section class="images mt-5 pt-5">
+        <div class="container-fluid pt-5">
+          <div class="row">
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/2104152/pexels-photo-2104152.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/110812/pexels-photo-110812.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/1133957/pexels-photo-1133957.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/2317904/pexels-photo-2317904.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/364086/pexels-photo-364086.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/1264210/pexels-photo-1264210.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/460775/pexels-photo-460775.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/416160/pexels-photo-416160.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/250716/pexels-photo-250716.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/169677/pexels-photo-169677.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/64219/dolphin-marine-mammals-water-sea-64219.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/372166/pexels-photo-372166.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/46231/water-lilies-pink-water-lake-46231.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/2317904/pexels-photo-2317904.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-6">
+              <div class="gallery-item mb-4">
+                <img src="https://images.pexels.com/photos/1683989/pexels-photo-1683989.jpeg?auto=compress&cs=tinysrgb&w=600"
+                  alt="" class="w-100 rounded"></img>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
   )
 }
